727573748896361422d2c8b7b445a247a88579a5
Jur de Vries <info@jurdevries.nl>
2024-12-13 18:18:59 +0100

    Refactored BalanceDrawer.ts.

--- src/BalanceDrawer.ts
+++ src/BalanceDrawer.ts
@@ -4,32 +4,45 @@ import {Balance} from "./balance";
 class BalanceDrawer {
     private p: p5;
     private balance: Balance;
+    private debitCreditWiths: number;
+    private offsetX: number;
+    private offsetY: number;
 
     constructor(p: p5, balance: Balance) {
         this.p = p;
         this.balance = balance;
+        this.debitCreditWiths = 100;
+        this.offsetX = 50;
+        this.offsetY = 50;
     }
 
 
     draw() {
         const p = this.p;
-        const left = 50;
-        const bottom = 50;
+        this.offsetY = p.height - 50;
+
+        p.text(`Balance: ${this.balance.name}`, this.offsetX, this.offsetY);
+
+        this.drawDebit();
+        this.drawCredit();
+    }
+
+    private drawDebit() {
         const totalDebits = Object.values(this.balance.debit).reduce((sum, value) => sum + value, 0);
+        const p = this.p;
+        let totalBalanceY = this.offsetY - 20 - totalDebits;
+        p.text('Debit', this.offsetX, totalBalanceY - 10);
+        p.rect(this.offsetX, totalBalanceY, this.debitCreditWiths, totalDebits);
+    }
+
+    private drawCredit() {
         const totalCredits = Object.values(this.balance.credit).reduce((sum, value) => sum + value, 0);
-
-        p.text(`Balance: ${this.balance.name}`, left, p.height - bottom);
-
-        let totalBalanceY = p.height - bottom - 20 - totalDebits;
-        let debitWidth = 100;
-        let creditWith = 100;
-        p.text('Debit', left, totalBalanceY - 10);
-        p.rect(left, totalBalanceY, debitWidth, totalDebits);
-
+        const p = this.p;
+        const creditLeft = this.offsetX + this.debitCreditWiths + 10;
+        let totalBalanceY = this.offsetY - 20 - totalCredits;
         p.fill(0, 100, 0);
-        const creditLeft = left + debitWidth + 10;
         p.text('Credit', creditLeft, totalBalanceY - 10);
-        p.rect(creditLeft, totalBalanceY, creditWith, totalCredits);
+        p.rect(creditLeft, totalBalanceY, this.debitCreditWiths, totalCredits);
     }
 }
 
