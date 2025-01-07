65432bb87edcc88a3ce4d509d8eed5a744a78d9d
Jur de Vries <info@jurdevries.nl>
2025-01-07 15:01:10 +0100

    Aanpassingen aan vormgeving.

--- src/balance.ts
+++ src/balance.ts
@@ -31,32 +31,54 @@ class Transaction {
     }
 }
 
+class BalanceStatus {
+    public inBalance: boolean = true;
+}
+
+type BalanceStatusCallback = (status: BalanceStatus) => void;
+
 class Balance {
     public name: string;
     public debit: { [key: string]: number };
     public credit: { [key: string]: number };
     private transactions: Transaction[];
+    private balanceStatus: BalanceStatus;
+    private totalDebit: number = 0;
+    private totalCredit: number = 0;
+    private balanceStatusCallback: BalanceStatusCallback | null = null;
 
     constructor(name: string) {
         this.name = name;
         this.debit = {};
         this.credit = {};
         this.transactions = [];
+        this.balanceStatus = new BalanceStatus();
     }
 
     addTransaction(transaction: Transaction) {
         this.transactions.push(transaction);
         if (transaction.debit.type != DebitTypes.none) {
-            this.debit[transaction.debit.type] = (this.debit[transaction.debit.type] || 0) + transaction.getAmount();            
+            this.debit[transaction.debit.type] = (this.debit[transaction.debit.type] || 0) + transaction.getAmount();
+            this.totalDebit += transaction.getAmount();
         }
         if (transaction.credit.type != CreditTypes.none) {
             this.credit[transaction.credit.type] = (this.credit[transaction.credit.type] || 0) + transaction.getAmount();
+            this.totalCredit += transaction.getAmount();
+        }
+        this.balanceStatus.inBalance = this.totalDebit == this.totalCredit;
+        if (this.balanceStatusCallback != null) {
+            this.balanceStatusCallback(this.balanceStatus);
         }
     }
 
     getLastTransaction() {
         return this.transactions[this.transactions.length - 1];
     }
+
+
+    registerBalanceStatusCallback(callback: (status: BalanceStatus) => void): void {
+        this.balanceStatusCallback = callback;
+    }
 }
 
-export { Balance, Transaction, DebitTypes, CreditTypes };
+export { Balance, Transaction, DebitTypes, CreditTypes, BalanceStatus, BalanceStatusCallback };
--- src/components/BalanceExplanationSketch.vue
+++ src/components/BalanceExplanationSketch.vue
@@ -1,51 +1,87 @@
 <template>
-  <p>
-    Om geldschepping te kunnen begrijpen wordt in de voorbeelden vaak gewerkt met een balans.
-    Je moet de balans dus begrijpen om verder te kunnen met de uitleg van geldschepping. We nemen een een persoon als voorbeeld.
-    In de economie wordt voor personen geen balans gebruikt, maar je kunt het daar net zo goed gebruiken.
-  </p>
-  <p>
-    In het begin van je leven heb je nog geen eigen geld. Pas als je wat ouder bent, krijg je voor het eerst zakgeld.
-    Dit is het begin van je leven als consument. Druk op de knop om Jantje zakgeld te geven (in cash).
-  </p>
-  <p><button @click="addPocketMoney" class="btn" :disabled="hasPocketMoneyBeenGiven">Geef € 50,- zakgeld in cash</button></p>
-  <p>Wat je ziet op de balans zijn twee kanten: de debet en de credit kant. Dat zijn wat moeilijke woorden.
-    <ul>
-      <li>Debet: ook wel activa of bezittingen genoemd: alles wat je (of een bedrijf) bezit</li>
-      <li>Credit: ook wel passiva of verplichtingen genoemd: dit is of eigen vermogen of schulden</li>
-    </ul>
-    <p>
-      <button @click="adjustPocketMoneyEquity" class="btn">Pas balans aan</button>
-    </p>
-    In het geval van je zakgeld krijg je dus € 50,- als cash wat op de linkerkant bij de bezittingen geplaatst wordt.
-    Aan de rechterkant (credit) zien we wat dit betekent voor je vermogen: dit gaat ook omhoog met € 50,-.
-  </p>
-  <p>
-    <button @click="makeAloan" class="btn">Leen € 50,- in cash om iets te kopen</button>
-  </p>
-  <div ref="canvasContainer"></div>
+  <div class="relative">
+    <transition>
+      <div v-if="currentStep == Step.STEP1">
+        <p>
+          Om geldschepping te kunnen begrijpen wordt in de voorbeelden vaak gewerkt met een balans.
+          Je moet de balans dus begrijpen om verder te kunnen met de uitleg van geldschepping. We nemen een een persoon als voorbeeld.
+          In de economie wordt voor personen geen balans gebruikt, maar je kunt het daar net zo goed gebruiken.
+        </p>
+        <p>
+          In het begin van je leven heb je nog geen eigen geld. Pas als je wat ouder bent, krijg je voor het eerst zakgeld.
+          Dit is het begin van je leven als consument. Druk op de knop om Jantje zakgeld te geven (in cash).
+        </p>
+        <p><button @click="addPocketMoney" class="btn" :disabled="hasPocketMoneyBeenGiven">Geef € 50,- zakgeld in cash</button></p>
+      </div>
+    </transition>
+    <transition>
+      <div v-if="currentStep == Step.STEP2">
+        <p>Wat je ziet op de balans zijn twee kanten: de debet en de credit kant. Dat zijn wat moeilijke woorden.
+          <ul>
+            <li>Debet: ook wel activa of bezittingen genoemd: alles wat je (of een bedrijf) bezit</li>
+            <li>Credit: ook wel passiva of verplichtingen genoemd: dit is of eigen vermogen of schulden</li>
+          </ul>
+          <button @click="adjustPocketMoneyEquity" class="btn">Pas balans aan</button>
+        </p>
+      </div>
+    </transition>
+    <transition>
+      <div v-if="currentStep == Step.STEP3">
+        <p>
+          In het geval van je zakgeld krijg je dus € 50,- als cash wat op de linkerkant bij de bezittingen geplaatst wordt.
+          Aan de rechterkant (credit) zien we wat dit betekent voor je vermogen: dit gaat ook omhoog met € 50,-.
+        </p>
+        <p>
+          <button @click="makeAloan" class="btn">Leen € 50,- in cash om iets te kopen</button>
+        </p>
+      </div>
+    </transition>
+  </div>
+  <div class="canvas" ref="canvasContainer">
+    <div v-if="!balanceIsBalanced" class="error balanceError">
+      De balans is niet in balans!
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onUnmounted } from "vue";
+import {defineComponent, onMounted, onUnmounted, ref} from "vue";
 import p5 from "p5";
 import oneBalanceSketch from "../sketches/oneBalanceSketch";
-import {Balance, CreditTypes, DebitTypes, Transaction} from "../balance";
+import {Balance, BalanceStatus, CreditTypes, DebitTypes, Transaction} from "../balance";
+
+enum Step {
+  STEP1,
+  STEP2,
+  STEP3
+}
 
 export default defineComponent({
   name: "BalanceExplanationSketch",
+
   setup() {
     const canvasContainer = ref<HTMLDivElement | null>(null);
+
+    let currentStep = ref<Step>(Step.STEP1);
 
     let p5Instance: p5 | null = null;
     const balance = new Balance("Jantje")
 
 
     const hasPocketMoneyBeenGiven = ref<boolean>(false);
+
+    const balanceIsBalanced = ref<boolean>(true);
+
+    const balanceStatusCallback = (balanceStatus: BalanceStatus) => {
+      balanceIsBalanced.value = balanceStatus.inBalance;
+    }
+
+    balance.registerBalanceStatusCallback(balanceStatusCallback);
+
     const addPocketMoney = () => {
       const transaction = new Transaction("First pocket money", 50, { type: DebitTypes.cash }, { type: CreditTypes.none });
       balance.addTransaction(transaction);
-      hasPocketMoneyBeenGiven.value = true;
+      currentStep.value = Step.STEP2;
     }
     const adjustPocketMoneyEquity = () => {
       const transaction = new Transaction("First pocket money equity", 50, { type: DebitTypes.none }, { type: CreditTypes.equity });
@@ -76,7 +112,10 @@ export default defineComponent({
       addPocketMoney,
       adjustPocketMoneyEquity,
       hasPocketMoneyBeenGiven,
+      balanceIsBalanced,
       makeAloan,
+      currentStep,
+      Step,
     };
   },
 });
@@ -84,11 +123,33 @@ export default defineComponent({
 
 <style scoped>
 /* Optional styling for the canvas container */
-div {
+.canvas {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
+  position: relative;
 }
+
+.balanceError {
+  position: absolute;
+  top: 10px;
+}
+
+.v-enter-from {
+  height: 0;
+  opacity: 0;
+  overflow: hidden;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: height 0.2s ease, opacity 1s ease;
+}
+.v-leave-to {
+  height: 0;
+  opacity: 0;
+}
+
 </style>
