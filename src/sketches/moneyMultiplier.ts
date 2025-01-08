b6150c80e6d14c6f5fd3062895c1b208bf4c014e
Jur de Vries <info@jurdevries.nl>
2025-01-08 17:45:01 +0100

    Added gsap animations using a timeline.

--- src/sketches/moneyMultiplier.ts
+++ src/sketches/moneyMultiplier.ts
@@ -1,13 +1,16 @@
 import p5 from "p5";
-import gsap from "gsap";
+import Timeline = gsap.core.Timeline;
 
+import gsapinstance from "gsap";
 import moneyIMage from "@/assets/images/money.png";
 
 class Money {
     private p: p5;
-    public position: { x: number; y: number } = { x: 0, y: 0 };
+    public position: { x: number; y: number } = {x: 0, y: 0};
+    public properties: { opacity: number } = {opacity: 0};
     private image: p5.Image;
     private width: number = 70;
+    private tl?: Timeline;
 
     constructor(p: p5, x: number, y: number) {
         this.p = p;
@@ -16,21 +19,53 @@ class Money {
         this.image = p.loadImage(moneyIMage, () => {
             this.image.resize(this.width, 0);
         });
+        this.tl = gsapinstance.timeline();
     }
 
     draw() {
-        this.p.image(this.image, this.position.x, this.position.y);
+        if (this.properties.opacity > 0) {
+            this.p.tint(255, 255, 255, this.properties.opacity * 255);
+            this.p.image(this.image, this.position.x, this.position.y);
+            this.p.tint(255, 255, 255, 255);
+        }
     }
 
     moveTo(x: number, y: number) {
-        gsap.to(this.position, {
+        const tl = gsapinstance.timeline();
+        this.tl?.to(this.position, {
             x: this.p.mouseX - this.width / 2,
             y: this.p.mouseY - this.width / 2,
             duration: 1,
-            ease: "power2.inOut",
+            ease: "sine.inOut",
         })
     }
+
+    moveToAndDisappear(x: number, y: number) {
+        this.tl?.clear();
+        this.properties.opacity = 0;
+        this.tl?.to(this.properties,
+            {
+                opacity: 1,
+                duration: 0.5,
+                ease: "sine.inOut",
+            });
+        this.tl?.to(this.position, {
+            x: this.p.mouseX - this.width / 2,
+            y: this.p.mouseY - this.width / 2,
+            duration: 1,
+            ease: "sine.inOut",
+        })
+        this.tl?.to(this.properties,
+            {
+                opacity: 0,
+                duration: 1,
+                ease: "sine.inOut",
+                delay: 0.4,
+            });
+
+    }
 }
+
 const createSketch = (canvasContainer: HTMLDivElement) => (p: p5) => {
     let canvas;
 
@@ -51,7 +86,7 @@ const createSketch = (canvasContainer: HTMLDivElement) => (p: p5) => {
     };
 
     p.mousePressed = () => {
-        money.moveTo(p.mouseX, p.mouseY);
+        money.moveToAndDisappear(p.mouseX, p.mouseY);
     }
 };
 
