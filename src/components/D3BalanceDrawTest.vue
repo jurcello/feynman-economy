c869532bd9038696def9fa0a670cef79aeb99f81
Jur de Vries <info@jurdevries.nl>
2025-03-25 15:16:20 +0100

    Add D3-based stacked bar chart for balance visualization
    Introduce D3 scales and stacking functionality to render debits and credits as a stacked bar chart. This enhances the component by visualizing balance data interactively, leveraging initial balance and color mappings for clarity.

--- src/components/D3BalanceDrawTest.vue
+++ src/components/D3BalanceDrawTest.vue
@@ -6,6 +6,8 @@
 <script setup lang="ts">
 import {Balance, Transaction, CreditTypes, DebitTypes, InitialBalance} from "@/balance";
 import {onMounted, ref} from "vue";
+import * as d3 from "d3"
+import {colorMappings} from "@/sketches/Animatables/BalanceDrawerExtended";
 
 const initialBalance: InitialBalance = {
   debit: [
@@ -19,14 +21,65 @@ const initialBalance: InitialBalance = {
   ]
 };
 
-// Create a new balance and add some transactions to it
 const balance = Balance.createFromInitialBalance('Barbie', initialBalance);
 
 const chart = ref<HTMLElement | null>(null);
 
+const WIDTH = 400;
+const HEIGHT = 300;
+
+const x = d3.scaleBand()
+    .domain(Object.keys(initialBalance))
+    .range([0, WIDTH])
+    .padding(0.1);
+
+const y = d3.scaleLinear()
+    .domain([0, balance.getTotalMoneyAggregates().total * 1.5])
+    .nice()
+    .range([HEIGHT, 0]);
+
+const color = d3.scaleOrdinal()
+    .domain(Object.keys(colorMappings))
+    .range(Object.values(colorMappings));
+
 
 onMounted(() => {
-  console.log(balance);
+  const data = balance.getTotalMoneyAggregates();
+
+  const stackedDebit = d3
+      .stack()
+      .keys(Object.keys(balance.debit))([balance.debit]);
+
+  const stackedCredit = d3
+      .stack()
+      .keys(Object.keys(balance.credit))([balance.credit]);
+
+  const svg = d3.select(chart.value)
+      .append("svg")
+      .attr("width", WIDTH)
+      .attr("height", HEIGHT);
+
+  svg.selectAll(".debit")
+      .data(stackedDebit)
+      .join("rect")
+      .attr("class", "debit")
+      .attr("fill", (d: any) => color(d.key) as string)
+      .attr("x", x('debit') as number)
+      .attr("y", (d: any) => y(d[0][1]))
+      .attr("width", x.bandwidth())
+      .attr("height", (d: any) => y(d[0][0]) - y(d[0][1]))
+
+  svg.selectAll(".credit")
+      .data(stackedCredit)
+      .join("rect")
+      .attr("class", "debit")
+      .attr("fill", (d: any) => color(d.key) as string)
+      .attr("x", x('credit') as number)
+      .attr("y", (d: any) => y(d[0][1]))
+      .attr("width", x.bandwidth())
+      .attr("height", (d: any) => y(d[0][0]) - y(d[0][1]));
+
+
 })
 </script>
 
