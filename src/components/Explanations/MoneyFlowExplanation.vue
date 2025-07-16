dec22bba85fb5992d1ec11ba8d9705b86ba14fec
Jur de Vries <info@jurdevries.nl>
2025-07-16 11:13:57 +0200

    Refactor `MoneyFlowExplanation` to reorganize `redrawBlocks` and `moveMoneyToWorkers` logic, improving initialization flow and function definitions.

--- src/components/Explanations/MoneyFlowExplanation.vue
+++ src/components/Explanations/MoneyFlowExplanation.vue
@@ -47,46 +47,13 @@ const world = new MoneyWorld(destinations);
 
 const canvas = ref<HTMLElement | null>(null);
 
-let moveMoneyToWorkers;
 
-const redrawBlocks = (svg: d3.Selection<SVGGElement, unknown, null, undefined>) => {
-  svg.selectAll("rect.money-block")
-      .data([...MoneyBlock.allBlocks], d => d.id)
-      .join(
-          enter => {
-            console.log("entering")
-            return enter
-                .append("rect")
-                .attr("class", "money-block")
-                .attr("fill", "red")
-                .attr("x", d => d.currentPosition.x)
-                .attr("y", d => d.currentPosition.y)
-                .attr("width", 10)
-                .attr("height", 10)
-          },
-          update => {
-            return update.call(
-                update => update.transition()
-                    .duration(1000)
-                    .delay((d, i) => i * 20)
-                    .attr("x", d => d.targetPosition.x > 0 ? d.targetPosition.x : d.currentPosition.x)
-                    .attr("y", d => d.targetPosition.y > 0 ? d.targetPosition.y : d.currentPosition.y)
-                    .end()
-                    .then(() => {
-                      MoneyBlock.updateMovingBlocks();
-                      console.log("money moved")
-                    })
-            )
-          },
-          exit => exit
-              // Animate the exit by shrinking height to 0
-              .call(exit => exit.transition()
-                  .duration(750)
-                  .attr("height", 0)
-                  .remove()
-              )
-      )
+let redrawBlocks: () => void;
+const moveMoneyToWorkers = () => {
+  company.moveTo(workers, 10);
+  redrawBlocks();
 }
+
 // Initialize money destinations
 onMounted(() => {
   const svg = d3.select(canvas.value)
@@ -105,12 +72,45 @@ onMounted(() => {
 
   const blocksGroup = svg.append("g");
 
-  redrawBlocks(blocksGroup)
-  moveMoneyToWorkers = () => {
-    company.moveTo(workers, 10);
-    redrawBlocks(blocksGroup);
+  redrawBlocks = () => {
+    svg.selectAll("rect.money-block")
+        .data([...MoneyBlock.allBlocks], d => d.id)
+        .join(
+            enter => {
+              console.log("entering")
+              return enter
+                  .append("rect")
+                  .attr("class", "money-block")
+                  .attr("fill", "red")
+                  .attr("x", d => d.currentPosition.x)
+                  .attr("y", d => d.currentPosition.y)
+                  .attr("width", 10)
+                  .attr("height", 10)
+            },
+            update => {
+              return update.call(
+                  update => update.transition()
+                      .duration(1000)
+                      .delay((_, i) => i * 20)
+                      .attr("x", d => d.targetPosition.x > 0 ? d.targetPosition.x : d.currentPosition.x)
+                      .attr("y", d => d.targetPosition.y > 0 ? d.targetPosition.y : d.currentPosition.y)
+                      .end()
+                      .then(() => {
+                        MoneyBlock.updateMovingBlocks();
+                        console.log("money moved")
+                      })
+              )
+            },
+            exit => exit
+                // Animate the exit by shrinking height to 0
+                .call(exit => exit.transition()
+                    .duration(750)
+                    .attr("height", 0)
+                    .remove()
+                )
+        )
   }
-
+  redrawBlocks()
 });
 
 // Clean up animation frame on component unmount
