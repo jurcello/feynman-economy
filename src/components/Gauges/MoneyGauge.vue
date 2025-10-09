a9ac88a8540f582b8fbc9618cb5b7079658af7ad
Jur de Vries <info@jurdevries.nl>
2025-10-09 17:40:23 +0200

    Enhance `Investment` view: add `MoneyGauge` component to display monetary values, update `PercentageGauge` with height adjustment.

--- src/components/Gauges/MoneyGauge.vue
+++ src/components/Gauges/MoneyGauge.vue
@@ -4,35 +4,30 @@
       :maxValue="max"
       :value="value"
       :width="200"
-      :height="130"
       :ringWidth="20"
-      :segments="3"
+      :segments="2"
       :segmentColors="colors"
-      :custom-segment-stops="[min, okThreshold, goodThreshold,max]"
-      :currentValueText="`${label} ${value}%`"
+      :custom-segment-stops="[min, value,max]"
+      :currentValueText="`${label} € ${value}`"
       :segmentValueFormatter="segmentFormatter"
   />
+
 </template>
 
 <script setup lang="ts">
 import Speedometer from 'vue-speedometer'
 // Only define the attributes (props) as requested
-const props = withDefaults(defineProps<{
+const props = defineProps<{
   min: number
   max: number
-  okThreshold?: number
-  goodThreshold?: number
   value: number
   label: string
-}>(), {
-  okThreshold: 5,
-  goodThreshold: 7,
-})
+}>()
 
-const colors = ['#DC3545', '#FFC107', '#28A745']
+const colors = ['#72b180', '#ffffff']
 
-const segmentFormatter = (value: number) => `${value}%`
-// No additional logic or rendering at this stage
+
+const segmentFormatter = (value: number) => `€ ${value}`
 </script>
 
 <style scoped>
--- src/components/ROC/SubComponents/Investment.vue
+++ src/components/ROC/SubComponents/Investment.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="investment-container">
     <h2>investment</h2>
+    <p>Totale lening: € 20.000.000,0</p>
     <PercentageGauge
         :min="0"
         :okThreshold="3.5"
@@ -9,12 +10,19 @@
         :value="5"
         label="ROC"
     />
+    <MoneyGauge
+        :min="0"
+        :max="10000"
+        :value="7000"
+        label="Var"
+      />
   </div>
 </template>
 
 <script setup lang="ts">
 
 import PercentageGauge from "@/components/Gauges/PercentageGauge.vue";
+import MoneyGauge from "@/components/Gauges/MoneyGauge.vue";
 </script>
 
 <style scoped>
