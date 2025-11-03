1742bd267097e46dfa992fd9c8f6edd09c09acf8
Jur de Vries <info@jurdevries.nl>
2025-11-03 10:56:49 +0100

    Added 2 destinations for private equity.

--- src/components/Explanations/PrivateEquityExplanation.vue
+++ src/components/Explanations/PrivateEquityExplanation.vue
@@ -1,9 +1,47 @@
 <template>
 <h1>private equity explanation</h1>
+  <MoneyFlowCanvas
+      :destinations="destinations"
+      :width="600"
+      :height="400"
+      :show-mouse-position="true"
+  />
 </template>
 
 <script setup lang="ts">
+import {MoneyDestination, MoneyDestinationConfig} from "@/utils/moneySquareUtils";
+import MoneyFlowCanvas from "@/components/Drawers/MoneyFlowCanvas.vue";
+import {ref} from "vue";
 
+const flowCanvas = ref<any>(null);
+const blockSize = 6;
+
+const configCompany = new MoneyDestinationConfig({
+  blockSize: blockSize,
+  blocksPerRow: 18,
+  blockGutter: 1,
+  position: {x: 250, y: 40},
+  showName: true,
+  scale: 0.7
+});
+const company = new MoneyDestination('Company', 0, configCompany);
+const configRevenue = new MoneyDestinationConfig({
+  blockSize: blockSize,
+  blocksPerRow: 18,
+  blockGutter: 1,
+  position: {x: 250, y: 120},
+  showName: true,
+  scale: 0.7
+});
+const revenue = new MoneyDestination('Revenue', 0, configRevenue);
+
+let redrawBlocks: () => void = () => {
+  flowCanvas.value?.redraw?.();
+
+
+};
+
+const destinations = [company, revenue];
 </script>
 
 <style scoped>
