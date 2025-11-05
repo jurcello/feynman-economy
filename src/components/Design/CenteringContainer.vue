93ebc63f7f84ce4543e56dceb8ef9122c1419199
Jur de Vries <info@jurdevries.nl>
2025-11-05 13:16:38 +0100

    Refactor PrivateEquityExplanation to use CenteringContainer and add reusable container component.

---
+++ src/components/Design/CenteringContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-2/3 mx-auto">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Explanations/PrivateEquityExplanation.vue
+++ src/components/Explanations/PrivateEquityExplanation.vue
@@ -1,37 +1,39 @@
 <template>
-  <h1>private equity explanation</h1>
-  <button
-      @click="executeTimelines"
-      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-  >
-    Start sequence
-  </button>
-  <button
-      @click="reset"
-      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-  >
-    Reset
-  </button>
-  <div class="visualisations">
-    <div class="left">
-      <PEPreLBO
-          ref="preLBO"
-      />
+  <centering-container>
+    <h1>private equity explanation</h1>
+    <button
+        @click="executeTimelines"
+        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+    >
+      Start sequence
+    </button>
+    <button
+        @click="reset"
+        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+    >
+      Reset
+    </button>
+    <div class="visualisations">
+      <div class="left">
+        <PEPreLBO
+            ref="preLBO"
+        />
+      </div>
+      <div class="right">
+        <PEPostLBO
+            ref="postLBO"
+        />
+      </div>
     </div>
-    <div class="right">
-      <PEPostLBO
-          ref="postLBO"
-      />
-    </div>
-  </div>
 
-
+  </centering-container>
 </template>
 
 <script setup lang="ts">
 import PEPreLBO from "@/components/Explanations/PrivateEquity/PEPreLBO.vue";
 import PEPostLBO from "@/components/Explanations/PrivateEquity/PEPostLBO.vue";
 import {ref} from "vue";
+import CenteringContainer from "@/components/Design/CenteringContainer.vue";
 
 const preLBO = ref<InstanceType<typeof PEPreLBO>>();
 const postLBO = ref<InstanceType<typeof PEPostLBO>>();
